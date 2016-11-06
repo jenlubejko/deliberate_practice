96e2705d88c37b7ed0a6788c2ffc79f379777bca
Jennifer Lubejko <jennifer.lubejko@gmail.com>
2016-11-06 13:45:48 -0600

    Add filter small numbers problem

---
+++ filter_small_numbers.rb
@@ -0,0 +1,9 @@
+# Filter out any numbers greater than 5 given the array [4, 82, 5, 1, 3, 2, 8].
+numbers = [4, 82, 5, 1, 3, 2, 8]
+small_numbers = []
+numbers.each do |number|
+  if number <= 5
+    small_numbers << number
+  end
+end 
+print small_numbers
